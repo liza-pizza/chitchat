b5d0a93548869a7e9da056b77d3328f1c8b2b72e
liza-pizza <mohitabipin@gmail.com>
2021-08-21 19:19:20 +0530

    add pod for title animation

--- chitchat/Controllers/HomeViewController.swift
+++ chitchat/Controllers/HomeViewController.swift
@@ -6,19 +6,36 @@
 //
 
 import UIKit
+import TweenKit
+
 
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
-    
+    let scheduler = ActionScheduler()    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+        titleLabel.text = "chatchit"
+            }
     
 
+    override func viewDidAppear(_ animated: Bool) {
+        
+            super.viewDidAppear(animated)
+            
+        
+        let action = InterpolationAction(from: "chatchit", to: "chitchat", duration: 0.7, easing: .exponentialInOut) { [unowned self] in
+                self.titleLabel.text = $0
+            }
+            
+           
+            let sequence = ActionSequence(actions: DelayAction(duration: 0.5), action, DelayAction(duration: 0.5))
+            
+        
+            scheduler.run(action: action)
+        }
+        
    
 
 }
