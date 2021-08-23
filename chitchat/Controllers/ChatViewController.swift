dffa2cabbb96b8da7f84985534f1058ee38a9dc5
liza-pizza <mohitabipin@gmail.com>
2021-08-23 20:22:55 +0530

    logout user

--- chitchat/Controllers/ChatViewController.swift
+++ chitchat/Controllers/ChatViewController.swift
@@ -6,15 +6,26 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
-
     
-
+    
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
     @IBAction func sendPressed(_ sender: Any) {
     }
     
+    @IBAction func logoutButtonPressed(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+        
+    }
 }
